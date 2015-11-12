2f8f3a9d1f0740d8981705798b21b096eccaf726
duyceuit <duyceuit@gmail.com>
2015-11-12 20:03:00 +0000

    modified:   GPIO_Control.cpp

--- GPIO_Control.cpp
+++ GPIO_Control.cpp
@@ -1,4 +1,4 @@
-//Edited
+// Will edit this file 2nd time
 /* A Simple GPIO application
 * Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
 * Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
